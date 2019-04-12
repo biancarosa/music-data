97c5fc1afb2860d4e677cb4c363a20acef54ecf6
Bianca Rosa <me@biancarosa.com.br>
2019-04-11 22:22:09 -0300

    :tada: initial commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/biancarosa/goasync/routes"
+)
+
+func main() {
+	e := echo.New()
+
+	// Setup Logrus
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.DebugLevel)
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", routes.HealthCheck)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
