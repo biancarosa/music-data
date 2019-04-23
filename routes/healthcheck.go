6c32698ee3bfd67ab01035ad47fe91b67e9322f4
Bianca Rosa <me@biancarosa.com.br>
2019-04-23 12:21:21 -0300

    add generated swagger

--- routes/healthcheck.go
+++ routes/healthcheck.go
@@ -9,41 +9,54 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-type applicationDetails struct {
-	// Número de go rotinas abertas
-	//
-	// Required: true
-	Goroutines int `json:"goroutines"`
-	// Quanto de memória está alocada para uso da aplicação
-	//
-	// Required: true
-	HeapAlloc string `json:"heapAlloc"`
-}
-
+// songResponse defines the response that you get of a valid healthcheck.
 // swagger:response healthCheckResponse
 type healthCheckResponse struct {
-	// Versão da aplicação
+	// The response for a valid healthcheck request.
 	//
-	// Required: true
-	ApplicationVersion string             `json:"applicationVersion"`
-	Application        applicationDetails `json:"application"`
+	// in: body
+	Body struct {
+		// Application version
+		//
+		// Required: true
+		ApplicationVersion string `json:"applicationVersion"`
+		Application        struct {
+			// Número de go rotinas abertas
+			//
+			// Required: true
+			Goroutines int `json:"goroutines"`
+			// Quanto de memória está alocada para uso da aplicação
+			//
+			// Required: true
+			HeapAlloc string `json:"heapAlloc"`
+		}
+	}
 }
 
 // HealthCheck is the route that prints a sucessful message when the application is fine.
-// swagger:route GET /healthcheck HealthCheck
-// Retorna informação da saúde da aplicação
-// responses:
-//  200: healthCheckResponse
 func HealthCheck(c echo.Context) error {
+	// swagger:route GET /healthcheck HealthCheck
+	//
+	// Returns information about our application health.
+	//
+	// We will check some info regarding the application health, including memory stats.
+	//
+	//     Produces:
+	//     - application/json
+	//
+	//     Schemes: https
+	//
+	//     Responses:
+	//     200: healthCheckResponse
 	log.Debug("Healthcheck has been called")
 	var mem runtime.MemStats
+	var resp healthCheckResponse
+
 	runtime.ReadMemStats(&mem)
-	apiResponse := &healthCheckResponse{
-		ApplicationVersion: "0.1.0", // #TODO Get from conf file
-		Application: applicationDetails{
-			Goroutines: runtime.NumGoroutine(),
-			HeapAlloc:  fmt.Sprintf("%d mb", mem.Sys/1000000), // bytes
-		},
-	}
-	return c.JSON(http.StatusOK, apiResponse)
+
+	resp.Body.ApplicationVersion = "0.1.0" // #TODO Get from conf file
+	resp.Body.Application.Goroutines = runtime.NumGoroutine()
+	resp.Body.Application.HeapAlloc = fmt.Sprintf("%d mb", mem.Sys/1000000) // bytes
+
+	return c.JSON(http.StatusOK, resp.Body)
 }
